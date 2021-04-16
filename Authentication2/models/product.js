9857087cc2ee004f990573281ec5d0bdfc477aae
rakhikrishna <rakhipms@gmail.com>
2021-04-16 17:35:51 +0530

    First Version

--- Authentication2/models/product.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-var productSchema = new mongoose.Schema({
-    name:String,
-    image:String,
-    price:Number
-})
-
-module.exports = mongoose.model("Product",productSchema);
