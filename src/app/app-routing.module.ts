e0a7d8f4e2801302e604fcc2eaa92ecac82a9419
rakhikrishna <rakhipms@gmail.com>
2021-04-16 17:57:46 +0530

    First version

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddauthorComponent } from './addauthor/addauthor.component';
+import { AddbookComponent } from './addbook/addbook.component';
+import { AuthorsComponent } from './authors/authors.component';
+import { BooksComponent } from './books/books.component';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { SigninComponent } from './signin/signin.component';
+
+const routes: Routes = [{path:'',component:HomeComponent},
+{path:'login',component:LoginComponent},
+{path:'signin',component:SigninComponent},
+{path:'addbook',component:AddbookComponent},
+{path:'addauthor',component:AddauthorComponent},
+{path:'books',component:BooksComponent},
+{path:'authors',component:AuthorsComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { SigninComponent } from './signin/signin.component';
+import { BooksComponent } from './books/books.component';
+import { AddbookComponent } from './addbook/addbook.component';
+import { AddauthorComponent } from './addauthor/addauthor.component';
+import { AuthorsComponent } from './authors/authors.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LoginComponent,
+    HomeComponent,
+    SigninComponent,
+    BooksComponent,
+    AddbookComponent,
+    AddauthorComponent,
+    AuthorsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
